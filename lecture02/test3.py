cbcc7f73f32ff9cc3199bc4c03256e7d6229c30e
venus240 <s6652410004@sau.ac.th>
2023-08-22 13:02:59 +0700

    code V.2: create 3 file

--- lecture02/test3.py
+++ lecture02/test3.py
@@ -1,1 +1,13 @@
-print('Hey...')
+emp_name = input("ป้อนชื่อพนักกงาน : ")
+sale_price = float(input("ป้อนยอดขาย : "))
+print('------------------')
+commission = float(sale_price) * 10/100
+s = format(float(sale_price),".2f")
+a = format(float(commission),".2f")
+print(f"คุณ {emp_name} ยอดขาย {sale_price:.2f} บาท ได้ค่าคอม {commission:.2f} บาท")
+
+print("คุณ",emp_name,"ยอดขาย",s,"บาท ได้ค่าคอม",a,"บาท")
+
+print("คุณ" +str(emp_name) +"ยอดขาย" +str(s) +"บาท ได้ค่าคอม" +str(a)+"บาท")
+
+print("คุณ {} ยอดขาย {:.2f} บาท ได้ค่าคอม {:.2f} บาท" . format(emp_name,sale_price,commission))
--- teat4.py
+++ teat4.py
@@ -1,1 +1,1 @@
-print()
+people = int
