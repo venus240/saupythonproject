08bfe455ad7c9b3f2287930faac8f6b10f22b90a
venus240 <142291020+venus240@users.noreply.github.com>
2023-08-28 22:36:15 +0700

    Update teat4.py

--- teat4.py
+++ teat4.py
@@ -1,1 +1,9 @@
-people = int
+people = int(input('จำนวนคน : '))
+money = float(input('จำนวนคน : '))
+
+result = float(money / people)
+a = format(float(result),".2f")
+print(f'หารคนละ {result:.2f} บาท')
+print('หารคนละ' ,a, 'บาท')
+print('หารคนละ'+' '+ str(a) +' '+ 'บาท')
+print('หารคนละ {:.2f} บาท'.format(result))
