6bdb6ce8dbfec902f4ff338d4bc0f74035fa6b7f
venus240 <142291020+venus240@users.noreply.github.com>
2023-09-04 11:03:45 +0700

    Delete saupythonproject-master/saupythonproject-master directory

--- saupythonproject-master/saupythonproject-master/lecture01/test1.py
+++
@@ -1,6 +0,0 @@
-print('Hi....')
-print(4562123)
-print('''
-      asdf
-      asdf
-      sdf''')
--- saupythonproject-master/saupythonproject-master/teat4.py
+++
@@ -1,9 +0,0 @@
-people = int(input('จำนวนคน : '))
-money = float(input('จำนวนคน : '))
-
-result = float(money / people)
-a = format(float(result),".2f")
-print(f'หารคนละ {result:.2f} บาท')
-print('หารคนละ' ,a, 'บาท')
-print('หารคนละ'+' '+ str(a) +' '+ 'บาท')
-print('หารคนละ {:.2f} บาท'.format(result))
