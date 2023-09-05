04d8af41353dd4cc82fe045b5d130271d9105ad4
venus240 <142291020+venus240@users.noreply.github.com>
2023-09-05 12:31:56 +0700

    Delete saupythonproject-master (1)/saupythonproject-master directory

--- saupythonproject-master (1)/saupythonproject-master/lecture04/dtitest07.py
+++
@@ -1,7 +0,0 @@
-id = input("รหัสพนักงาน : ")
-name = input("ชื่อพนักงาน : ")
-salary =float(input("เงินเดือนปกติของพนักงาน"))
-
-total = salary - (salary * 7 / 100) - 500
-
-print(f'เงินเดือนสุทธิ {total:.2f} บาท ')
