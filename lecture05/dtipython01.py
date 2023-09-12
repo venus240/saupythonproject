50d0b94ca91b80345927e2a2707961ea7e58197a
venus240 <142291020+venus240@users.noreply.github.com>
2023-09-12 11:04:13 +0700

    Add files via upload

---
+++ lecture05/dtipython01.py
@@ -0,0 +1,21 @@
+#Fuction 1: no paramiter/no return ไม่มีอะไรในวงเล็บ/ไม่มี
+def funcA( ) :
+    print("AAA")
+    print("BBB")
+    #ไม่มีคำสั่ง return
+
+def funcB( ):
+    print(111)
+    funcA( )
+
+
+funcA( )
+funcA( )
+funcB( )
+funcA( )
+
+
+
+
+
+
---
+++ lecture06/dtipython05.py
@@ -0,0 +1,9 @@
+#คำสั่ง while
+
+x = 1
+
+while x <= 5 :
+    print("DTI")
+    x = x + 1
+
+print("hello")
