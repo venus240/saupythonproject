912e60c5c4239825f1c1237f5de27e3ee180bfd8
venus240 <142291020+venus240@users.noreply.github.com>
2023-09-19 22:51:36 +0700

    Add files via upload

--- workshop/workshop06.py
+++ workshop/workshop06.py
@@ -1,1 +1,9 @@
 name = input("ป้อนชื่อผู้กู้ : ")
+quantity = int(input("จำนวนเงินกู้ : "))
+
+
+if quantity >= 1000 :
+    print(f'อัตราดอกเบี้ย {quantity * 2.5 / 100}บาท ')
+
+else :
+    print(f'อัตราดอกเบี้ย {quantity * 5.5 / 100}บาท ')
