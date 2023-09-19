3ef47ab68da6bc2022aaff97b950723eaef2bf1b
venus240 <142291020+venus240@users.noreply.github.com>
2023-09-19 11:05:36 +0700

    Add files via upload

---
+++ workshop/workshop13.py
@@ -0,0 +1,9 @@
+number = input("รหัสนักเรียน : ")
+name = input("ชื่อนักเรียน : ")
+grade = float(input("เกรดเฉลี่ย : "))
+
+if grade >= 2 :
+    print('ผ่าน')
+
+elif grade < 2 :
+    print('ไม่ผ่าน')
