e0908d40fae17d511123a762439f49d30c2cbd20
venus240 <142291020+venus240@users.noreply.github.com>
2023-10-10 12:25:35 +0700

    Add files via upload

---
+++ lecture07/test07.py
@@ -0,0 +1,13 @@
+# การเข้าถึงทุกข้อมูลใน list and tuple
+    #   0   1   2       3       4           5
+var1 = [10,20,"hello",True, [111,"wow"], "มานะ"]
+
+var2 = (10,20,"hello",True, (111,"wow"), "มานะ")
+
+for data in var1 : #data = ตัวแปร
+        print(data)
+
+for data in var2 : #data = ตัวแปร
+        print(data)
+
+
