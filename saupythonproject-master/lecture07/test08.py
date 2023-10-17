8de9f543393bbedc77c6d9388cf8690469f47adb
venus240 <142291020+venus240@users.noreply.github.com>
2023-10-17 12:12:11 +0700

    Delete saupythonproject-master directory

--- saupythonproject-master/lecture07/test08.py
+++
@@ -1,7 +0,0 @@
-var2 = (10,20,"hello",True, (111,"wow"), "มานะ")
-
-#การเปลี่ยนแปลงค่า เพิ่ม ลบ ข้อมูลใน Tuple
-# list(), tuple()
-varTemp = list(var2)
-varTemp[2] = "Hi"
-print(var2)
--- saupythonproject-master/workshop/workshop07.py
+++
@@ -1,7 +0,0 @@
-number = input("ป้อนตัวเลขที่ต้องการทาย : ")
-
-if number == 25 :
-    print ("Correct, You are the winner")
-
-else :
-    print ("Not Correct !!!")
